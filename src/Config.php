83a699f549bda2178b0b937cbe37c192232e9e9a
rvogel <vogel@hallowelt.biz>
2018-12-07 16:26:38 +0100

    Add UserInfoModifier infrastructure
    Change-Id: I42b95b5ae26f477f9b129b3bd7aba2d57ae63446

--- src/Config.php
+++ src/Config.php
@@ -5,4 +5,5 @@ namespace MediaWiki\Extension\LDAPUserInfo;
 class Config {
 	const DOMAINCONFIG_SECTION = 'userinfo';
 	const ATTRIBUTES_MAP = 'attributes-map';
+	const GENERIC_PROPERTY_NORMALIZATION_CALLBACKS = 'generic-property-normalization-callbacks';
 }
