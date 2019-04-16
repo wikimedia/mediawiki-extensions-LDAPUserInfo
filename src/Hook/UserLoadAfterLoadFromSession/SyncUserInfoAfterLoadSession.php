28245346c728500b9ec9b46c7fa9d42f2953f700
oleksandrpinchuk <intracomof@gmail.com>
2019-04-16 20:06:14 +0300

    UserInfo Sync refactoring
    UserInfoSync encapsulated to the Process that
    can be used by Hook
    
    Deleted UserLoggedIn Hook
    
    Fixed Codestyle issues
    
    Change-Id: I4a2be11ead67605a55f9f7509127e555d02e948e
    Task: T220471

---
+++ src/Hook/UserLoadAfterLoadFromSession/SyncUserInfoAfterLoadSession.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace MediaWiki\Extension\LDAPUserInfo\Hook\UserLoadAfterLoadFromSession;
+
+use MediaWiki\Extension\LDAPProvider\Hook\UserLoadAfterLoadFromSession;
+use MediaWiki\Extension\LDAPUserInfo\UserInfoSyncProcess;
+use MediaWiki\Extension\LDAPUserInfo\Config;
+use User;
+
+class SyncUserInfoAfterLoadSession extends UserLoadAfterLoadFromSession {
+
+	protected $sessionDataKey = 'ldap-user-sync-last';
+
+	/**
+	 * @return bool
+	 * @throws \ConfigException
+	 */
+	protected function doSync() {
+		$process = new UserInfoSyncProcess(
+			$this->user,
+			$this->domainConfig,
+			$this->ldapClient,
+			$this->config->get( 'LDAPUserInfoModifierRegistry' )
+		);
+
+		$process->run();
+
+		return true;
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	protected function getDomainConfigSection() {
+		return Config::DOMAINCONFIG_SECTION;
+	}
+
+}
